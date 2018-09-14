767200359214f543658f0ab6e2d90a3bc90567f1
Daniel Becker <danielbecker@Daniels-MacBook-Pro.local>
2018-09-14 06:49:44 -0600

    wdi9-w0104-homework

---
+++ javascript/wdi9-wd0104-homework.js
@@ -0,0 +1,55 @@
+// const checkPalindrome = (string) => {
+//     let reverseWord = string.split('').reverse().join('');
+//     if(reverseWord === string){
+//         console.log('true');
+//     } else {
+//         console.log('false');
+//     }
+// }
+
+// console.log(checkPalindrome('racecar'));
+// console.log(checkPalindrome('hannah'));
+// console.log(checkPalindrome('tacos'));
+
+
+//?????
+// const sumDigits = (num1) => {
+//     let digitString = num1.split(' ');
+//     console.log(digitString.reduce((a, b) => a + b));
+// }
+
+// console.log(sumDigits('1234')); 
+
+
+
+// const calculateSide = (sideA, sideB) => {
+//     const sideC = (sideA * sideA) + (sideB * sideB);
+//     console.log(Math.sqrt(sideC));
+// }
+
+// console.log(calculateSide(5, 4));
+
+
+
+// ???????
+// const sumArray = (arr) => {
+//     for(let i = 0; i < arr.lenth; sum += arr[i++]);
+//     console.log(sum);
+// }
+
+// console.log([1,2,3]);
+
+
+
+// ???????
+// const checkPrime = (limit) => {
+//     const limitArray = limit.split('');
+//     for(let i = 0; i < limitArray.length; i++){
+//         if(i % 1 === 1) {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// console.log(checkPrime('20'));
+
